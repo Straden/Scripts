34674010a8e33bd2820804f53af879018e0d6df8
Straden <60278089+Straden@users.noreply.github.com>
2021-11-14 17:43:08 -0500

    Update and rename stronccMain to stronccMain.lua

--- stronccMain.lua
+++ stronccMain.lua
@@ -2,7 +2,8 @@ if not isfile("StronccHub") then
     makefolder("StronccHub")
 end
 local gameToLink = {
-    [2377868063] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccStrucid"
+    [2377868063] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccStrucid",
+    [3233893879] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccBadBusiness.lua"
 }
 if not gameToLink[game.PlaceId] then game.Players.LocalPlayer:Kick("Universal is still in development") end
 loadstring(game:HttpGet((gameToLink[game.PlaceId]),true))()
