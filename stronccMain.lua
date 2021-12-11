2fa4be36ede4f820666a548d25c7dbcb14787b92
Straden <60278089+Straden@users.noreply.github.com>
2021-12-11 14:51:49 -0500

    Update stronccMain.lua

--- stronccMain.lua
+++ stronccMain.lua
@@ -3,7 +3,8 @@ if not isfile("StronccHub") then
 end
 local gameToLink = {
     [2377868063] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccStrucid",
-    [3233893879] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccBadBusiness.lua"
+    [3233893879] = "https://raw.githubusercontent.com/Straden/Scripts/main/stronccBadBusiness.lua",
+    [6808416928] = "https://raw.githubusercontent.com/Straden/Scripts/main/aimblox.lua"
 }
 if not gameToLink[game.PlaceId] then game.Players.LocalPlayer:Kick("Universal is still in development") end
 loadstring(game:HttpGet((gameToLink[game.PlaceId]),true))()
